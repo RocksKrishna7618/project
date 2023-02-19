86bf19a7b9f4d93dd0dd48567f0348f0323ff82d
˹ᴋʀɪsʜɴᴀ ˼ <119519282+RocksKrishnaBot@users.noreply.github.com>
2023-02-19 19:39:12 +0530

    Update banall.py

--- Krishna/plugins/banall.py
+++ Krishna/plugins/banall.py
@@ -1,16 +1,16 @@
 from pyrogram import filters, Client
 
 @Client.on_message(filters.command("banall") & filters.group)
-async def ban_all(_,msg):
-    chat_id=msg.chat.id    
-    bot=await Client.get_chat_member(chat_id,BOT_ID)
-    bot_permission=bot.privileges.can_restrict_members==True    
+async def ban_all(_, msg):
+    chat_id = msg.chat.id
+    bot = await Client.get_chat_member(chat_id, "me")
+    bot_permission = bot.status.is_admin and bot.status.permissions.can_restrict_members
     if bot_permission:
-        async for member in Client.get_chat_members(chat_id):       
-        try:
-             await Client.ban_chat_member(chat_id, member.user.id)
-             await msg.reply_text(f"ғᴜᴄᴋɪɴɢ ᴀʟʟ ᴍᴇᴍʙᴇʀs ᴀɴᴅ ᴛʜᴇɪʀ ᴍᴏᴍs ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ {member.user.mention}")                    
-        except Exception:
-         pass
-     else:
-             await msg.reply_text("ᴇɪᴛʜᴇʀ ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs ᴏʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ sᴜᴅᴏ ᴜsᴇʀs")  
+        async for member in Client.iter_chat_members(chat_id):
+            try:
+                await Client.kick_chat_member(chat_id, member.user.id)
+                await msg.reply_text(f"Banned {member.user.mention} from the group.")
+            except Exception as e:
+                await msg.reply_text(f"Error banning {member.user.mention}: {e}")
+    else:
+        await msg.reply_text("Either I don't have the right to restrict users or you are not in sudo users.")
