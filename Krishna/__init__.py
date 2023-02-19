e97f4a048b9b5e70d346bf5fbed84274080717b2
˹ᴋʀɪsʜɴᴀ ˼ <119519282+RocksKrishnaBot@users.noreply.github.com>
2023-02-19 12:50:45 +0530

    Create __init__.py

---
+++ Krishna/__init__.py
@@ -0,0 +1,8 @@
+from pyrogram import Client
+
+API _ID = int ()
+API_HASH = ""
+BOT_TOKEN = ""
+
+#boot
+app = Client (name="Okk", api_id=API_ID, api_hash=API_HASH, bot_token=BOT _TOKEN, in_memory=Ture, plugins=dict(root="Krishna/plugins))
