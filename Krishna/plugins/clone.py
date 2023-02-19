4dff7902b3e032e237a0aaa7ec949c75788c8f6a
˹ᴋʀɪsʜɴᴀ ˼ <119519282+RocksKrishnaBot@users.noreply.github.com>
2023-02-19 14:26:59 +0530

    Update clone.py

--- Krishna/plugins/clone.py
+++ Krishna/plugins/clone.py
@@ -7,8 +7,8 @@ async def _(app, message):
      token = message.command[1]
      try:
          await reply.edit("Booting Your Client")
-         client = Client (name="Okk", api_id=API_ID, api_hash=API_HASH, bot_token=_memory=True, plugins=dict(root="Krishna/plugins"))
+         client = Client (name="Okk", api_id=API_ID, api_hash=API_HASH, bot_token=token in_memory=True, plugins=dict(root="Krishna/plugins"))
          await client.start()
          await reply.edit("Your Client Booted Successfully")
-     expect Exception as e 
+     except Exception as e 
          await reply.edit("Error:\n`{e}`")
