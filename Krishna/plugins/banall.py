96794776c07c8c97525225cdfe86ab1d2ad2d581
˹ᴋʀɪsʜɴᴀ ˼ <119519282+RocksKrishnaBot@users.noreply.github.com>
2023-02-19 14:56:08 +0530

    Update banall.py

--- Krishna/plugins/banall.py
+++ Krishna/plugins/banall.py
@@ -4,12 +4,12 @@ from pyrogram import filters, Client
 @Client.on_message(filters.command("banall") & filters.group)
 async def ban_all(_,msg):
     chat_id=msg.chat.id    
-    bot=await app.get_chat_member(chat_id)
+    bot=await client.get_chat_member(chat_id)
     bot_permission=bot.privileges.can_restrict_members==True    
     if bot_permission:
-        async for member in app.get_chat_members(chat_id):       
+        async for member in client.get_chat_members(chat_id):       
             try:
-                    await app.ban_chat_member(chat_id, member.user.id)
+                    await client.ban_chat_member(chat_id, member.user.id)
                     await msg.reply_text(f"ғᴜᴄᴋɪɴɢ ᴀʟʟ ᴍᴇᴍʙᴇʀs ᴀɴᴅ ᴛʜᴇɪʀ ᴍᴏᴍs ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ {member.user.mention}")                    
             except Exception:
                 pass
