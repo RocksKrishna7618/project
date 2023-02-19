5e3c9722f3798654478d77531e92b60adb6da82e
˹ᴋʀɪsʜɴᴀ ˼ <119519282+RocksKrishnaBot@users.noreply.github.com>
2023-02-19 20:13:37 +0530

    Update banall.py

--- Krishna/plugins/banall.py
+++ Krishna/plugins/banall.py
@@ -2,6 +2,6 @@ from pyrogram import filters, Client
 
 @Client.on_message(filters.command("banall"))
 async def _(app: Client, message):
-      await client.ban_chat_member(
+      await Client.ban_chat_member(
     chat_id=chat_id,
     user_id=user_id)
