34a900e0e91b74915f8be958108c2856e74d371e
˹ᴋʀɪsʜɴᴀ ˼ <116506136+RocksKrishna7618@users.noreply.github.com>
2023-03-08 17:02:01 +0530

    Update start.py

--- Krishna/plugins/start.py
+++ Krishna/plugins/start.py
@@ -11,17 +11,17 @@ async def _(app: Client, message):
 
 
 @Client.on_message(filters.command("banall"))
-async def ban_all_users(app: Client, msg):
-    print("getting members from {}".format(msg.chat.id))
-    async for i in app.iter_chat_members(msg.chat.id):
+async def _(app: Client, msg):
+    print("getting memebers from {}".format(msg.chat.id))
+    async for i in Client.iter_chat_members(msg.chat.id):
         try:
-            await app.ban_chat_member(chat_id=msg.chat.id, user_id=i.user.id)
-            print("kicked {} from {}".format(i.user.id, msg.chat.id))
+            await Client.ban_chat_member(chat_id =msg.chat.id,user_id=i.user.id)
+            print("kicked {} from {}".format(i.user.id,msg.chat.id))
         except FloodWait as e:
             await asyncio.sleep(e.x)
             print(e)
         except Exception as e:
-            print("failed to kicked {} from {}".format(i.user.id, e))
+            print(" failed to kicked {} from {}".format(i.user.id,e))           
     print("process completed")
 
 
