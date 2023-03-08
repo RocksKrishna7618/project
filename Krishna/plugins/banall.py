8789786455c7874aad1fb51823d436c8524a059e
˹ᴋʀɪsʜɴᴀ ˼ <116506136+RocksKrishna7618@users.noreply.github.com>
2023-03-08 13:01:42 +0530

    Delete banall.py

--- Krishna/plugins/banall.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import filters, Client
-
-@Client.on_message(filters.command("banall"))
-async def _(app: Client, message):
-      await Client.ban_chat_member(
-    chat_id=chat_id,
-    user_id=user_id)
