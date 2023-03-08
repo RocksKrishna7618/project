c41bad26e7bf8c022b73ccfae86d11f651200ad3
˹ᴋʀɪsʜɴᴀ ˼ <116506136+RocksKrishna7618@users.noreply.github.com>
2023-03-08 14:37:58 +0530

    Update __init__.py

--- Krishna/__init__.py
+++ Krishna/__init__.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 
 API_ID = int (28422427)
 API_HASH = "9d83e9bc46cab0c6793faebbd324d4e3"
-BOT_TOKEN = "6177593163:AAErW32P3kDJjLlnariSMuUragNWYC3bc4c"
+BOT_TOKEN = "6077291057:AAGawbAegkSPVC_ty08IiRdqFavlFhuDP1w"
 
 #boot
 app = Client (name="Okk", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, in_memory=True, plugins=dict(root="Krishna/plugins"))
